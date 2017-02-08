78d388fcece2fe0cc4f2b219440718860ea3eff8
tizah <davidzagi93@gmail.com>
2017-02-08 12:51:46 +0100

    Created the student Controller and the view

--- SwiftSkool.MVC5/ViewModels/StudentInputModel.cs
+++ SwiftSkool.MVC5/ViewModels/StudentInputModel.cs
@@ -5,29 +5,21 @@ using System.Collections.Generic;
 
 namespace SwiftSkool.MVC5.ViewModels
 {
-    public class StudentInputModel
+    public class CreateStudentInputModel
     {
-        public string StudentID { get; set; }
-        public bool Activate { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string OtherName { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string Email { get; set; }
-        public int Age { get; set; }
-        public string Gender { get; set; }
-        public Address Address { get; set; }
-        public string State { get; set; }
-        public Attendance Attendance { get; set; }
-        public string Class { get; set; }
-        public string Club { get; set; }
-        public string Hostel { get; set; }
-        public string MedicalHistory { get; set; }
-        public string StudentPassPort { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Country { get; set; }
+        public Guardian guardian { get; set; } 
+    }
+    public class GuardianViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public AddressViewModel Address { get; set; }
+    }
+    public class AddressViewModel
+    {
         public string City { get; set; }
-        public Guardian Guardian { get; set; }
-        public List<Subject> Subjects { get; set; }
+        public string NameOfArea { get; set; }
     }
 }
