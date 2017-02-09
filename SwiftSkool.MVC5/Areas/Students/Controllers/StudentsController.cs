95cb77a432b2ab75ec756d63649ddd65d80bfb40
tizah <davidzagi93@gmail.com>
2017-02-09 11:56:38 +0100

    changes to the student Controller

--- SwiftSkool.MVC5/Areas/Students/Controllers/StudentsController.cs
+++ SwiftSkool.MVC5/Areas/Students/Controllers/StudentsController.cs
@@ -27,8 +27,8 @@ namespace SwiftSkool.MVC5.Areas.Students.Controllers
         // GET: Students/Students
         public async Task<ActionResult> Index()
         {
-            var students = await _studQry
-            return View();
+            var students = await _studQry.GetStudentsAsync();
+            return View(students);
         }
 
         // GET: Students/Students/Details/5
